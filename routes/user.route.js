49269e36f12022910ee655f7320940a3db895537
Angie Molina <amolina1795@gmail.com>
2025-10-02 18:43:09 -0500

    Add JWT authentication to user routes for enhanced security

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const jwtAuth = require('../middlewares/auth/jwt.auth');
 const { validate } = require('../middlewares/validations');
 const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/user.controller');
 const { getUserByIdValidation, createUserValidation, updateUserValidation } = require('../middlewares/validations/user.validation');
 
 router.post('/', validate(createUserValidation), createUser);
 
-router.get('/:id', validate(getUserByIdValidation), getUserById);
-router.put('/:id', validate(getUserByIdValidation), updateUser);
-router.delete('/:id', validate(getUserByIdValidation), deleteUser);
-router.get('/', getAllUsers);
+router.get('/:id', jwtAuth, validate(getUserByIdValidation), getUserById);
+router.put('/:id', jwtAuth, validate(getUserByIdValidation), updateUser);
+router.delete('/:id', jwtAuth, validate(getUserByIdValidation), deleteUser);
+router.get('/', jwtAuth, getAllUsers);
 
 
 module.exports = router;
